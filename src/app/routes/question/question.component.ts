43bd93d9ecbbe949ed7ef5b6809dc081d907ba70
charly <charly333@free.fr>
2019-06-13 15:04:31 +0200

    next question > score page ok

--- src/app/routes/question/question.component.ts
+++ src/app/routes/question/question.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizzService } from 'src/app/quizz.service';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -11,13 +12,31 @@ export class QuestionComponent implements OnInit {
 
   f = new FormGroup({
     answer: new FormControl('', Validators.required)
-   });
+  });
 
-constructor(
-  public quizz: QuizzService
-) { }
+  constructor(
+    public quizz: QuizzService,
+    private router: Router
+  ) { }
 
-ngOnInit() {
-}
+  ngOnInit() {
+  }
+
+  submit() {
+    // console.log('next question');
+    if (this.quizz.current.questions[this.quizz.progress.questionId].correctAnswer === this.f.value.answer) {
+      this.quizz.progress.score++;
+    } else {
+      this.quizz.progress.score--;
+    }
+    this.quizz.progress.questionId++;
+    this.quizz.syncProgress();
+    if (this.quizz.progress.questionId === this.quizz.current.questions.length) {
+      this.router.navigateByUrl('/score');
+      return;
+    }
+    this.f.reset();
+  }
+
 
 }
