a28bce2a22fdf0bcbc3530c54d68fd1a76f5e3b6
charly <charly333@free.fr>
2019-06-14 13:48:00 +0200

    test OK ter

--- src/app/quizz.service.spec.ts
+++ src/app/quizz.service.spec.ts
@@ -1,11 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { QuizzService } from './quizz.service';
 
 describe('QuizzService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-
-  }));
+  beforeEach(() => TestBed.configureTestingModule({  }));
 
   it('should be created', () => {
     const service: QuizzService = TestBed.get(QuizzService);
